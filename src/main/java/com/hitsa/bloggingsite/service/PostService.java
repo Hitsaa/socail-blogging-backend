5ef52f0dee6a8359604887830fc287becd9bc590
hitsaa <sshitendra010@gmail.com>
2021-04-28 21:52:47 +0530

    created al services

---
+++ src/main/java/com/hitsa/bloggingsite/service/PostService.java
@@ -0,0 +1,74 @@
+package com.hitsa.bloggingsite.service;
+
+import com.hitsa.bloggingsite.dto.PostRequest;
+import com.hitsa.bloggingsite.dto.PostResponse;
+import com.hitsa.bloggingsite.exceptions.PostNotFoundException;
+import com.hitsa.bloggingsite.exceptions.SubredditNotFoundException;
+import com.hitsa.bloggingsite.mapper.PostMapper;
+import com.hitsa.bloggingsite.model.Post;
+import com.hitsa.bloggingsite.model.Subreddit;
+import com.hitsa.bloggingsite.model.User;
+import com.hitsa.bloggingsite.repository.PostRepository;
+import com.hitsa.bloggingsite.repository.SubredditRepository;
+import com.hitsa.bloggingsite.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+@Transactional
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final SubredditRepository subredditRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final PostMapper postMapper;
+
+    public void save(PostRequest postRequest) {
+        Subreddit subreddit = subredditRepository.findByName(postRequest.getSubredditName())
+                .orElseThrow(() -> new SubredditNotFoundException(postRequest.getSubredditName()));
+        postRepository.save(postMapper.map(postRequest, subreddit, authService.getCurrentUser()));
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponse getPost(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(id.toString()));
+        return postMapper.mapToDto(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getAllPosts() {
+        return postRepository.findAll()
+                .stream()
+                .map(postMapper::mapToDto)
+                .collect(toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsBySubreddit(Long subredditId) {
+        Subreddit subreddit = subredditRepository.findById(subredditId)
+                .orElseThrow(() -> new SubredditNotFoundException(subredditId.toString()));
+        List<Post> posts = postRepository.findAllBySubreddit(subreddit);
+        return posts.stream().map(postMapper::mapToDto).collect(toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return postRepository.findByUser(user)
+                .stream()
+                .map(postMapper::mapToDto)
+                .collect(toList());
+    }
+}
---
+++ src/main/java/com/hitsa/bloggingsite/service/SubredditService.java
@@ -0,0 +1,45 @@
+package com.hitsa.bloggingsite.service;
+
+import com.hitsa.bloggingsite.dto.SubredditDto;
+import com.hitsa.bloggingsite.exceptions.SpringRedditException;
+import com.hitsa.bloggingsite.mapper.SubredditMapper;
+import com.hitsa.bloggingsite.model.Subreddit;
+import com.hitsa.bloggingsite.repository.SubredditRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class SubredditService {
+
+    private final SubredditRepository subredditRepository;
+    private final SubredditMapper subredditMapper;
+
+    @Transactional
+    public SubredditDto save(SubredditDto subredditDto) {
+        Subreddit save = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
+        subredditDto.setId(save.getId());
+        return subredditDto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubredditDto> getAll() {
+        return subredditRepository.findAll()
+                .stream()
+                .map(subredditMapper::mapSubredditToDto)
+                .collect(toList());
+    }
+
+    public SubredditDto getSubreddit(Long id) {
+        Subreddit subreddit = subredditRepository.findById(id)
+                .orElseThrow(() -> new SpringRedditException("No subreddit found with ID - " + id));
+        return subredditMapper.mapSubredditToDto(subreddit);
+    }
+}
