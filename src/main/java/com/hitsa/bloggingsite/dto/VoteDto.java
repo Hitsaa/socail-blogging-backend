4a4fd7baacfc491dd9d1980755dd4760b16accc4
hitsaa <sshitendra010@gmail.com>
2021-04-28 21:54:13 +0530

    created new dto package and classes

---
+++ src/main/java/com/hitsa/bloggingsite/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.hitsa.bloggingsite.dto;
+
+import com.hitsa.bloggingsite.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
