6c89c79a5b796712360f90018e466b3fde499022
hitsaa <sshitendra010@gmail.com>
2021-04-28 21:55:24 +0530

    created mapper package and classes

---
+++ src/main/java/com/hitsa/bloggingsite/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.hitsa.bloggingsite.mapper;
+
+import com.hitsa.bloggingsite.dto.CommentsDto;
+import com.hitsa.bloggingsite.model.Comment;
+import com.hitsa.bloggingsite.model.Post;
+import com.hitsa.bloggingsite.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    @Mapping(target = "user", source = "user")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
